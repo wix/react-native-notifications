9199941f6616775e0e1553d616494aa537eb58a1
Jonathan Stanton <jonathan@jastanton.com>
2017-01-18 13:07:48 -0800

    mimick iOs behavior of posting notifications to notification center

--- android/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
+++ android/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
@@ -67,7 +67,9 @@ public class PushNotification implements IPushNotification {
 
     @Override
     public void onReceived() throws InvalidNotificationException {
-        postNotification(null);
+        if (!mAppLifecycleFacade.isAppVisible()) {
+            postNotification(null);
+        }
         notifyReceivedToJS();
     }
 
