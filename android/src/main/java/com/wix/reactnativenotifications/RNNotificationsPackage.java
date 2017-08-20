289f23c305de891c48d806b586ee94db8a476d4d
Jan Söndermann <jan.soendermann+git@gmail.com>
2017-08-20 16:58:59 +0800

    :alien: Make this package work with react native 0.47

--- android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
+++ android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
@@ -27,11 +27,6 @@ public class RNNotificationsPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
