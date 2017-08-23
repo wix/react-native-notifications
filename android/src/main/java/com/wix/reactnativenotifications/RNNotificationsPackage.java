1a841ac85493d57fdd7c48c7bab4a1c3bd735a98
zimzim62000 <zimzim62000@gmail.com>
2017-08-23 14:26:01 +0200

    can't overide if no extend!
    remove all overide of that pakage

--- android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
+++ android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
@@ -21,17 +21,14 @@ public class RNNotificationsPackage implements ReactPackage {
         mApplication = application;
     }
 
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Arrays.<NativeModule>asList(new RNNotificationsModule(mApplication, reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
