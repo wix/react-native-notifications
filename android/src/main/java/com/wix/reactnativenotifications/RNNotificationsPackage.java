193c0881e47b344e0e53ff3769b444cacf02ed76
Keshav Kumar <keshav@getservice.com>
2017-09-29 17:46:17 -0700

    remove @override create js module, 0.47 breaking change

--- android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
+++ android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
@@ -26,7 +26,7 @@ public class RNNotificationsPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new RNNotificationsModule(mApplication, reactContext));
     }
 
-    @Override
+    // Deprecated in 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
