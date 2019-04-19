wix/react-native-notifications

commit 731cc93bd44d0d8e385491a59f6c2ff4ad81f74d

Verify FCM notification intent

android/src/main/java/com/wix/reactnativenotifications/RNNotificationsModule.java
@@ -65,7 +65,9 @@ public class RNNotificationsModule extends ReactContextBaseJavaModule implements
         Bundle notificationData = intent.getExtras();
         if (notificationData != null) {
             final IPushNotification notification = PushNotification.get(getReactApplicationContext().getApplicationContext(), notificationData);
-            notification.onOpened();
+            if (notification != null) {
+                notification.onOpened();
+            }
         }
     }
 

android/src/main/java/com/wix/reactnativenotifications/gcm/FcmInstanceIdListenerService.java
@@ -21,8 +21,7 @@ public class FcmInstanceIdListenerService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage message){
-        Map data = message.getData();
-        Bundle bundle = convertMapToBundle(data);
+        Bundle bundle = message.toIntent().getExtras();
         Log.d(LOGTAG, "New message from GCM: " + bundle);
 
         try {
@@ -33,14 +32,4 @@ public class FcmInstanceIdListenerService extends FirebaseMessagingService {
             Log.v(LOGTAG, "GCM message handling aborted", e);
         }
     }
-
-    private Bundle convertMapToBundle(Map<String, String> map) {
-        Bundle bundle = new Bundle();
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            bundle.putString(entry.getKey(), entry.getValue());
-        }
-
-        return bundle;
-    }
-
 }

android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
@@ -63,11 +63,14 @@ public class RNNotificationsPackage implements ReactPackage, AppLifecycleFacade.
         final IPushNotificationsDrawer notificationsDrawer = PushNotificationsDrawer.get(mApplication.getApplicationContext());
         notificationsDrawer.onNewActivity(activity);
 
-        Bundle notificationData = activity.getIntent().getExtras();
-        if (notificationData != null) {
-            final IPushNotification pushNotification = PushNotification.get(mApplication.getApplicationContext(), notificationData);
-            if (pushNotification != null) {
-                pushNotification.onOpened();
+        Intent intent = activity.getIntent();
+        if (intent != null) {
+            Bundle notificationData = intent.getExtras();
+            if (notificationData != null) {
+                final IPushNotification pushNotification = PushNotification.get(mApplication.getApplicationContext(), notificationData);
+                if (pushNotification != null) {
+                    pushNotification.onOpened();
+                }
             }
         }
     }

android/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
@@ -43,6 +43,10 @@ public class PushNotification implements IPushNotification {
     };
 
     public static IPushNotification get(Context context, Bundle bundle) {
+        if (verifyNotificationBundle(bundle) == false) {
+            return null;
+        }
+
         Context appContext = context.getApplicationContext();
         if (appContext instanceof INotificationsApplication) {
             return ((INotificationsApplication) appContext).getPushNotification(context, bundle, AppLifecycleFacadeHolder.get(), new AppLaunchHelper());
@@ -56,6 +60,14 @@ public class PushNotification implements IPushNotification {
         mAppLaunchHelper = appLaunchHelper;
         mJsIOHelper = JsIOHelper;
         mNotificationProps = createProps(bundle);
+    }
+
+    private static boolean verifyNotificationBundle(Bundle bundle) {
+        if (bundle.getString("google.message_id") != null) {
+            return true;
+        }
+
+        return false;
     }
 
     @Override

commit 1c3072505ae4bbcaea68ba1bc5ab65b2f55e4176

Fix possible NPE when notification bundle verification fails
When the notification bundle verification introduced by commit 731cc93bd44d0d8e385491a59f6c2ff4ad81f74d
returns false, PushNotification.get returns null. This commit adds null checks to
invocations of that method.

android/src/main/java/com/wix/reactnativenotifications/RNNotificationsModule.java
@@ -99,7 +99,9 @@ public class RNNotificationsModule extends ReactContextBaseJavaModule implements
         Log.d(LOGTAG, "Native method invocation: postLocalNotification");
         final Bundle notificationProps = Arguments.toBundle(notificationPropsMap);
         final IPushNotification pushNotification = PushNotification.get(getReactApplicationContext().getApplicationContext(), notificationProps);
-        pushNotification.onPostRequest(notificationId);
+        if (pushNotification != null) {
+            pushNotification.onPostRequest(notificationId);
+        }
     }
 
     @ReactMethod

android/src/main/java/com/wix/reactnativenotifications/gcm/FcmInstanceIdListenerService.java
@@ -26,7 +26,9 @@ public class FcmInstanceIdListenerService extends FirebaseMessagingService {
 
         try {
             final IPushNotification notification = PushNotification.get(getApplicationContext(), bundle);
-            notification.onReceived();
+            if (notification != null) {
+                notification.onReceived();
+            }
         } catch (IPushNotification.InvalidNotificationException e) {
             // A GCM message, yes - but not the kind we know how to work with.
             Log.v(LOGTAG, "GCM message handling aborted", e);