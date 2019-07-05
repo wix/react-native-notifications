9cf0127a4865729de786ce3464c2e53e273dd450
Vijay Sapkota <bvsapkota@gmail.com>
2019-07-05 14:31:27 +0200

    fix import

--- android/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
+++ android/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
@@ -4,8 +4,11 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
