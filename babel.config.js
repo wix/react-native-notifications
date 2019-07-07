bb5b455ec5c577f696650f90dc0333f3218ac2d5
yogevbd <yogev132@gmail.com>
2019-07-07 16:12:33 +0300

    Fix tests

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,11 @@ module.exports = function (api) {
   api && api.cache(false);
   return {
     presets: [
-      "module:metro-react-native-babel-preset"
+      'module:metro-react-native-babel-preset'
     ],
     plugins: [
-      "@babel/plugin-proposal-export-namespace-from",
-      "@babel/plugin-proposal-export-default-from"
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-export-default-from'
     ]
   };
-}
+};
