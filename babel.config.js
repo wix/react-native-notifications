961f9a8ba8685700cba4e0f67bc2b58356ad27e9
yogevbd <yogev132@gmail.com>
2019-07-07 20:23:59 +0300

    Remove babel preset env

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,7 @@ module.exports = function (api) {
   api && api.cache(false);
   return {
     presets: [
-      'module:metro-react-native-babel-preset',
-      '@babel/preset-env'
+      'module:metro-react-native-babel-preset'
     ],
     plugins: [
       '@babel/plugin-proposal-export-namespace-from',
