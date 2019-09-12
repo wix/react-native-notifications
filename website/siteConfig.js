f56e53fe2fba6b71f554086b83c40647bdc2ce00
yogevbd <yogev132@gmail.com>
2019-09-12 11:48:56 +0300

    3.0.0-alpha.0

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'react-native-notifications',
-  organizationName: 'Wix',
+  organizationName: 'wix',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
