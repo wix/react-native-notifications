d5631a8fa0681e51d68de8489c2cbd599dcb5d29
Carlos Alcazar <carlos.alcazar@caresource.com>
2019-09-19 15:33:23 -0400

    Not sure how long this error has been here but we are importing the wrong thing. We should be importing NonNull as importing Nullable causes a build error. The RN 60 flavor of the file is importing the NonNull

--- android/app/src/reactNative59/java/com/wix/reactnativenotifications/NotificationManagerCompatFacade.java
+++ android/app/src/reactNative59/java/com/wix/reactnativenotifications/NotificationManagerCompatFacade.java
@@ -2,7 +2,7 @@
 package com.wix.reactnativenotifications;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationManagerCompat;
 
 public abstract class NotificationManagerCompatFacade {
