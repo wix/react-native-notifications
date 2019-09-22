85a117e9489a77ef01df2d7d766b41d6f89f288d
yogevbd <yogev132@gmail.com>
2019-09-22 12:15:20 +0300

    Auto generate changelog

---
+++ .grenrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  template: {
+    commit: ({message, url, author, name}) => `- [${message}](${url}) - ${author ? `@${author}` : name}`,
+    issue: "- {{name}} [{{text}}]({{url}})",
+    label: "[**{{label}}**]",
+    noLabel: "closed",
+    group: "\n#### {{heading}}\n",
+    changelogTitle: "# Changelog\n\n",
+    release: "## {{release}} ({{date}})\n{{body}}",
+    releaseSeparator: "\n---\n\n"
+  },
+  groupBy: {
+    "Enhancements:": ["enhancement", "internal"],
+    "Bug Fixes:": ["bug"],
+    "Features": ["feature"]
+  },
+  ignoreIssuesWith: [
+    "skip-changelog"
+  ],
+  ignoreTagsWith: [
+    "snapshot"
+  ],
+  dataSource: "prs",
+  changelogFileName: "CHANGELOG.gren.md",
+  tags: "all",
+  override: true,
+  generate: true
+}
