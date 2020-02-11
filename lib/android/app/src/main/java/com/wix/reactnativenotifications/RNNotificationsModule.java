f1e74f401a35b84b90eb2a704c04922effbff5c3
Miguel Alatzar <this.migbot@gmail.com>
2020-02-11 15:30:41 -0700

    Use JobIntentService over IntentService

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/RNNotificationsModule.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/RNNotificationsModule.java
@@ -131,6 +131,6 @@ public class RNNotificationsModule extends ReactContextBaseJavaModule implements
         final Context appContext = getReactApplicationContext().getApplicationContext();
         final Intent tokenFetchIntent = new Intent(appContext, FcmInstanceIdRefreshHandlerService.class);
         tokenFetchIntent.putExtra(extraFlag, true);
-        appContext.startService(tokenFetchIntent);
+        FcmInstanceIdRefreshHandlerService.enqueueWork(appContext, tokenFetchIntent);
     }
 }
--- lib/android/app/src/main/java/com/wix/reactnativenotifications/fcm/FcmInstanceIdRefreshHandlerService.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/fcm/FcmInstanceIdRefreshHandlerService.java
@@ -1,19 +1,22 @@
 package com.wix.reactnativenotifications.fcm;
 
-import android.app.IntentService;
+import android.support.annotation.NonNull;
+import android.support.v4.app.JobIntentService;
+import android.content.Context;
 import android.content.Intent;
 
-public class FcmInstanceIdRefreshHandlerService extends IntentService {
+public class FcmInstanceIdRefreshHandlerService extends JobIntentService {
 
     public static String EXTRA_IS_APP_INIT = "isAppInit";
     public static String EXTRA_MANUAL_REFRESH = "doManualRefresh";
+    static final int JOB_ID = 1000;
 
-    public FcmInstanceIdRefreshHandlerService() {
-        super(FcmInstanceIdRefreshHandlerService.class.getSimpleName());
+    public static void enqueueWork(Context context, Intent work) {
+        enqueueWork(context, FcmInstanceIdRefreshHandlerService.class, JOB_ID, work);
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
+    protected void onHandleWork(@NonNull Intent intent) {
         IFcmToken fcmToken = FcmToken.get(this);
         if (fcmToken == null) {
             return;
