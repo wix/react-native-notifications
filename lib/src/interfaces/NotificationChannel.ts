6a8161b8bc3a9ec9035e5e6f3d6809c93fa7632a
Buicescu Alexandru <alexbuicescu@users.noreply.github.com>
2020-04-08 12:56:54 +0300

    Channels (#535)
    * creating channels works
    
    * check if app is visible when showing notification
    
    * changed protected method to private
    
    * fixes imports in typescript
    
    * fixes push notifications tests
    
    * rerun tests - mac slave went offline

---
+++ lib/src/interfaces/NotificationChannel.ts
@@ -0,0 +1,39 @@
+export class NotificationChannel {
+  channelId: string
+  name: string
+  importance: -1000 | 0 | 1 | 2 | 3 | 4 | 5
+  description?: string
+  enableLights?: boolean
+  enableVibration?: boolean
+  groupId?: string
+  lightColor?: string
+  showBadge?: boolean
+  soundFile?: string // "sound_file.mp3" for the file "android/app/src/main/res/raw/sound_file.mp3"
+  vibrationPattern?: number[]
+
+  constructor(
+    channelId: string,
+    name: string,
+    importance: -1000 | 0 | 1 | 2 | 3 | 4 | 5,
+    description?: string,
+    enableLights?: boolean,
+    enableVibration?: boolean,
+    groupId?: string,
+    lightColor?: string,
+    showBadge?: boolean,
+    soundFile?: string,
+    vibrationPattern?: number[],
+  ) {
+    this.channelId = channelId;
+    this.name = name;
+    this.importance = importance;
+    this.description = description;
+    this.enableLights = enableLights;
+    this.enableVibration = enableVibration;
+    this.groupId = groupId;
+    this.lightColor = lightColor;
+    this.showBadge = showBadge;
+    this.soundFile = soundFile;
+    this.vibrationPattern = vibrationPattern;
+  }
+}
