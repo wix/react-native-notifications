76c2d13274aa060584c9895f787008be553fbbd4
Alexander Anisimov <anisimov74@gmail.com>
2020-04-20 17:29:22 +0200

    fix: do not clear other notifications on open a notification (#559)
    * fix: do not clear other notifications on open a notification
    
    * fix: typo

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
@@ -68,7 +68,6 @@ public class PushNotification implements IPushNotification {
     @Override
     public void onOpened() {
         digestNotification();
-        clearAllNotifications();
     }
 
     @Override
@@ -193,11 +192,6 @@ public class PushNotification implements IPushNotification {
     protected void postNotification(int id, Notification notification) {
         final NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(id, notification);
-    }
-
-    protected void clearAllNotifications() {
-        final NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.cancelAll();
     }
 
     protected int createNotificationId(Notification notification) {
