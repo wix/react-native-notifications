09dbfcf5ba903e25a83b5fba3e642d3b2731ca40
Oleg Blednov <oleg@blednov.me>
2020-06-20 08:23:36 -0400

    When the app is in background, registerNotificationOpened wasn't triggered when user presses on notification banner. See https://github.com/wix/react-native-notifications/pull/614/files

--- android/app/src/main/java/com/wix/reactnativenotifications/core/NotificationIntentAdapter.java
+++ android/app/src/main/java/com/wix/reactnativenotifications/core/NotificationIntentAdapter.java
@@ -23,6 +23,9 @@ public class NotificationIntentAdapter {
         if (intent != null) {
             Bundle notificationData = intent.getExtras();
             if (notificationData != null && intent.hasExtra(PUSH_NOTIFICATION_EXTRA_NAME)) {
+            if (notificationData != null &&
+                    (intent.hasExtra(PUSH_NOTIFICATION_EXTRA_NAME) ||
+                            notificationData.getString("google.message_id", null) != null)) {
                 return true;
             }
         }
