54d6bc28abae301ed6466cc019a694028993b7a4
Henrik Bjørnskov <19725+henrikbjorn@users.noreply.github.com>
2020-07-22 15:03:48 +0200

    Pushkit completion handler (#644)
    Make use of `RNNotificationsStore` for PushKit notifications and implement the new `PushKit` received handler `withCompletionHandler`.
    
    It compiles and runs the ios e2e tests.

--- lib/ios/RNPushKitEventHandler.m
+++ lib/ios/RNPushKitEventHandler.m
@@ -1,14 +1,25 @@
 #import "RNPushKitEventHandler.h"
 #import "RNEventEmitter.h"
 
-@implementation RNPushKitEventHandler
+@implementation RNPushKitEventHandler {
+  RNNotificationsStore* _store;
+}
+
+- (instancetype)initWithStore:(RNNotificationsStore *)store {
+  self = [super init];
+  _store = store;
+  return self;
+}
 
 - (void)registeredWithToken:(NSString *)token {
     [RNEventEmitter sendEvent:RNPushKitRegistered body:@{@"pushKitToken": token}];
 }
 
-- (void)didReceiveIncomingPushWithPayload:(NSDictionary *)payload {
-    [RNEventEmitter sendEvent:RNPushKitNotificationReceived body:payload];
+- (void)didReceiveIncomingPushWithPayload:(NSDictionary *)payload withCompletionHandler:(void (^)(void))completionHandler {
+  NSString *completionKey = [payload objectForKey:@"uuid"];
+
+  [_store setActionCompletionHandler:completionHandler withCompletionKey:completionKey];
+  [RNEventEmitter sendEvent:RNPushKitNotificationReceived body:payload];
 }
 
 @end
