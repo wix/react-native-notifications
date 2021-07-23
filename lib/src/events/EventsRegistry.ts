b7ef39f5859deba9844d23d9ada36919b9763e13
Julien Brayere <julien.brayere@gmail.com>
2021-07-23 16:40:53 +0200

    fixing callback for registerNotificationReceivedBackground

--- lib/src/events/EventsRegistry.ts
+++ lib/src/events/EventsRegistry.ts
@@ -1,36 +1,36 @@
 import { EmitterSubscription } from 'react-native';
-import { NativeEventsReceiver } from '../adapters/NativeEventsReceiver';
-import {
-  Registered,
-  RegistrationError
-} from '../interfaces/NotificationEvents';
-import { NotificationActionResponse } from '../interfaces/NotificationActionResponse';
 import { CompletionCallbackWrapper } from '../adapters/CompletionCallbackWrapper';
+import { NativeEventsReceiver } from '../adapters/NativeEventsReceiver';
 import { Notification } from '../DTO/Notification';
-import { NotificationCompletion, NotificationBackgroundFetchResult } from '../interfaces/NotificationCompletion';
+import { NotificationActionResponse } from '../interfaces/NotificationActionResponse';
+import { NotificationCompletion } from '../interfaces/NotificationCompletion';
+import {
+    Registered,
+    RegistrationError
+} from '../interfaces/NotificationEvents';
 
 export class EventsRegistry {
   constructor(
     private nativeEventsReceiver: NativeEventsReceiver,
-    private completionCallbackWrapper: CompletionCallbackWrapper) 
+    private completionCallbackWrapper: CompletionCallbackWrapper)
   {}
 
   public registerRemoteNotificationsRegistered(callback: (event: Registered) => void): EmitterSubscription {
     return this.nativeEventsReceiver.registerRemoteNotificationsRegistered(callback);
   }
-  
+
   public registerNotificationReceivedForeground(callback: (notification: Notification, completion: (response: NotificationCompletion) => void) => void): EmitterSubscription {
     return this.nativeEventsReceiver.registerNotificationReceived(this.completionCallbackWrapper.wrapReceivedForegroundCallback(callback));
   }
 
-  public registerNotificationReceivedBackground(callback: (notification: Notification, completion: (response: NotificationBackgroundFetchResult) => void) => void): EmitterSubscription {
+  public registerNotificationReceivedBackground(callback: (notification: Notification, completion: (response: NotificationCompletion) => void) => void): EmitterSubscription {
     return this.nativeEventsReceiver.registerNotificationReceivedBackground(this.completionCallbackWrapper.wrapReceivedBackgroundCallback(callback));
   }
-  
+
   public registerNotificationOpened(callback: (notification: Notification, completion: () => void, actionResponse?: NotificationActionResponse) => void): EmitterSubscription {
     return this.nativeEventsReceiver.registerNotificationOpened(this.completionCallbackWrapper.wrapOpenedCallback(callback));
   }
-  
+
   public registerRemoteNotificationsRegistrationFailed(callback: (event: RegistrationError) => void): EmitterSubscription {
     return this.nativeEventsReceiver.registerRemoteNotificationsRegistrationFailed(callback);
   }
