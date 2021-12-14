eb60d6b257466d627eba25b3b6f74abd2aab243f
Igor Gnatyuk <igorgn@wix.com>
2021-12-14 13:55:19 +0200

    add nvmrc and comment out publishing

--- scripts/release.js
+++ scripts/release.js
@@ -95,7 +95,7 @@ function tryPublishAndTag(version) {
 function tagAndPublish(newVersion) {
     console.log(`trying to publish ${newVersion}...`);
     exec.execSync(`npm --no-git-tag-version version ${newVersion}`);
-    exec.execSync(`npm publish --tag ${VERSION_TAG}`);
+    // exec.execSync(`npm publish --tag ${VERSION_TAG}`);
     exec.execSync(`git tag -a ${newVersion} -m "${newVersion}"`);
     exec.execSyncSilent(`git push deploy ${newVersion} || true`);
     if (isRelease) {
