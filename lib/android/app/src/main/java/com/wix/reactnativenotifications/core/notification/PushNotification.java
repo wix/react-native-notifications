63ec3ca757e657ab3f823702b4e1633a74e9c7f9
Raviraj <ivarjar22@yahoo.com>
2023-05-18 13:02:57 +0530

    Init (#920)

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
@@ -97,22 +97,16 @@ public class PushNotification implements IPushNotification {
     protected void digestNotification() {
         if (!mAppLifecycleFacade.isReactInitialized()) {
             setAsInitialNotification();
-            launchOrResumeApp();
             return;
         }
 
         final ReactContext reactContext = mAppLifecycleFacade.getRunningReactContext();
         if (reactContext.getCurrentActivity() == null) {
             setAsInitialNotification();
+            return;
         }
 
-        if (mAppLifecycleFacade.isAppVisible()) {
-            dispatchImmediately();
-        } else if (mAppLifecycleFacade.isAppDestroyed()) {
-            launchOrResumeApp();
-        } else {
-            dispatchUponVisibility();
-        }
+        dispatchImmediately();
     }
 
     protected PushNotificationProps createProps(Bundle bundle) {
@@ -127,17 +121,6 @@ public class PushNotification implements IPushNotification {
         notifyOpenedToJS();
     }
 
-    protected void dispatchUponVisibility() {
-        mAppLifecycleFacade.addVisibilityListener(getIntermediateAppVisibilityListener());
-
-        // Make the app visible so that we'll dispatch the notification opening when visibility changes to 'true' (see
-        // above listener registration).
-        launchOrResumeApp();
-    }
-
-    protected AppVisibilityListener getIntermediateAppVisibilityListener() {
-        return mAppVisibilityListener;
-    }
 
     protected Notification buildNotification(PendingIntent intent) {
         return getNotificationBuilder(intent).build();
@@ -210,13 +193,6 @@ public class PushNotification implements IPushNotification {
         response.putBundle("notification", mNotificationProps.asBundle());
 
         mJsIOHelper.sendEventToJS(NOTIFICATION_OPENED_EVENT_NAME, response, mAppLifecycleFacade.getRunningReactContext());
-    }
-
-    protected void launchOrResumeApp() {
-        if (NotificationIntentAdapter.canHandleTrampolineActivity(mContext)) {
-            final Intent intent = mAppLaunchHelper.getLaunchIntent(mContext);
-            mContext.startActivity(intent);
-        }
     }
 
     private int getAppResourceId(String resName, String resType) {
