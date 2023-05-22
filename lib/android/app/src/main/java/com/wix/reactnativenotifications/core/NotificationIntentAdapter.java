102d64ff6cbf4bf5f13f23184efbbdccbdd7af1d
DanielEliraz <danielel@wix.com>
2023-05-22 13:46:31 +0300

    Revert "Fix for Android trampoline restriction" (#969)
    * Revert "Init (#920)"
    
    This reverts commit 63ec3ca757e657ab3f823702b4e1633a74e9c7f9.
    
    * fix-ios-test
    
    * remove jenkins leftovers
    
    * bump iphone
    
    * enlarge java heap

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/core/NotificationIntentAdapter.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/core/NotificationIntentAdapter.java
@@ -1,9 +1,10 @@
 package com.wix.reactnativenotifications.core;
 
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 
 import com.wix.reactnativenotifications.core.notification.PushNotificationProps;
@@ -11,14 +12,27 @@ import com.wix.reactnativenotifications.core.notification.PushNotificationProps;
 public class NotificationIntentAdapter {
     private static final String PUSH_NOTIFICATION_EXTRA_NAME = "pushNotification";
 
+    @SuppressLint("UnspecifiedImmutableFlag")
     public static PendingIntent createPendingNotificationIntent(Context appContext, PushNotificationProps notification) {
-        Intent intent = new AppLaunchHelper().getLaunchIntent(appContext);
-        intent.putExtra(PUSH_NOTIFICATION_EXTRA_NAME, notification.asBundle());
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return PendingIntent.getActivity(appContext, (int) System.currentTimeMillis(), intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+        if (canHandleTrampolineActivity(appContext)) {
+            Intent intent = new Intent(appContext, ProxyService.class);
+            intent.putExtra(PUSH_NOTIFICATION_EXTRA_NAME, notification.asBundle());
+            return PendingIntent.getService(appContext, (int) System.currentTimeMillis(), intent, PendingIntent.FLAG_ONE_SHOT);
         } else {
-            return PendingIntent.getActivity(appContext, (int) System.currentTimeMillis(), intent, PendingIntent.FLAG_ONE_SHOT);
+            Intent mainActivityIntent = appContext.getPackageManager().getLaunchIntentForPackage(appContext.getPackageName());
+            mainActivityIntent.putExtra(PUSH_NOTIFICATION_EXTRA_NAME, notification.asBundle());
+            TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(appContext);
+            taskStackBuilder.addNextIntentWithParentStack(mainActivityIntent);
+            return taskStackBuilder.getPendingIntent((int) System.currentTimeMillis(), PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
         }
+    }
+
+    public static boolean canHandleTrampolineActivity(Context appContext) {
+        return android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.R || appContext.getApplicationInfo().targetSdkVersion < 31;
+    }
+
+    public static Bundle extractPendingNotificationDataFromIntent(Intent intent) {
+        return intent.getBundleExtra(PUSH_NOTIFICATION_EXTRA_NAME);
     }
 
     public static boolean canHandleIntent(Intent intent) {
--- scripts/test-unit.js
+++ scripts/test-unit.js
@@ -14,38 +14,38 @@ function run() {
 
 function runAndroidUnitTests() {
   const conf = release ? 'testReactNative60ReleaseUnitTest' : 'testReactNative60DebugUnitTest';
-  if (android && process.env.JENKINS_CI) {
-    const sdkmanager = '/usr/local/share/android-sdk/tools/bin/sdkmanager';
-    exec.execSync(`yes | ${sdkmanager} --licenses`);
-    // exec.execSync(`echo y | ${sdkmanager} --update && echo y | ${sdkmanager} --licenses`);
-  }
   exec.execSync(`cd lib/android && ./gradlew ${conf}`);
 }
 
 function runIosUnitTests() {
-  const conf = release ? `Release` : `Debug`;
+  exec.execSync('npm run build');
   exec.execSync('npm run pod-install');
+  testTarget('NotificationsExampleApp', 'iPhone 12', '14.4');
+}
+
+function testTarget(scheme, device, OS = 'latest') {
+  const conf = release ? `Release` : `Debug`;
   exec.execSync(`cd ./example/ios &&
             RCT_NO_LAUNCH_PACKAGER=true
             xcodebuild build build-for-testing
-            -scheme "NotificationsExampleApp"
+            -scheme "${scheme}"
             -workspace NotificationsExampleApp.xcworkspace
             -sdk iphonesimulator
             -configuration ${conf}
-            -derivedDataPath ./example/ios/DerivedData/NotificationsExampleApp
+            -derivedDataPath ./DerivedData/NotificationsExampleApp
             -quiet
-            -UseModernBuildSystem=NO
+            -UseModernBuildSystem=YES
             ONLY_ACTIVE_ARCH=YES`);
 
   exec.execSync(`cd ./example/ios &&
             RCT_NO_LAUNCH_PACKAGER=true
             xcodebuild test-without-building
-            -scheme "NotificationsExampleApp"
+            -scheme "${scheme}"
             -workspace NotificationsExampleApp.xcworkspace
             -sdk iphonesimulator
             -configuration ${conf}
-            -destination 'platform=iOS Simulator,name=iPhone 11'
-            -derivedDataPath ./example/ios/DerivedData/NotificationsExampleApp
+            -destination 'platform=iOS Simulator,name=${device},OS=${OS}'
+            -derivedDataPath ./DerivedData/NotificationsExampleApp
             ONLY_ACTIVE_ARCH=YES`);
 }
 
